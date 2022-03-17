e30c59b32586edbeb69350e85901068c914c1250
sh <dltngusd2@naver.com>
2022-03-17 11:18:55 +0900

    a

--- src/java_test/A_Branch.java
+++ src/java_test/A_Branch.java
@@ -2,6 +2,6 @@ package java_test;
 
 public class A_Branch {
 	public static void main(String[] args) {
-		System.out.println("a-branch 기능추가");
+		System.out.println("a-branch");
 	}
 }
--- src/java_test/CommonService.java
+++ src/java_test/CommonService.java
@@ -3,5 +3,7 @@ package java_test;
 public class CommonService {
 	public static void main(String[] args) {
 		System.out.println("공통기능추가");
+		System.out.println("a-branch기능추가");
 	}
 }
+
